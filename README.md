a6f5b7bcff050d0155f025ee4916ae8cbb9b318b
Andrei Goncharenko <andrei.h@seranking.com>
2025-03-20 18:46:22 +0100

    add sniffer

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/J-u-i-c-y/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/J-u-i-c-y/php-project-45/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/e78acbd132247bfe7f39/maintainability)](https://codeclimate.com/github/J-u-i-c-y/php-project-45/maintainability)
--- src/Cli.php
+++ src/Cli.php
@@ -2,11 +2,12 @@
 
 namespace Src\Cli;
 
-use function cli\line; 
+use function cli\line;
 use function cli\prompt;
 
-function welcome() { 
-    line('Welcome to the Brain Game!'); 
-    $name = prompt('May I have your name?'); 
-    line("Hello, %s!", $name); 
+function welcome()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }

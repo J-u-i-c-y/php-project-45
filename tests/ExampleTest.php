3536d7cba225c6c21a1ab0477e9ca680f4f7d982
Andrei Goncharenko <andrei.h@seranking.com>
2025-07-28 17:37:08 +0300

    fix bugs

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,8 +6,14 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public function isEven(int $num): bool
+    {
+        return $num % 2 === 0;
+    }
+
     public function testExample(): void
     {
-        $this->assertTrue(true);
+        $this->assertTrue($this->isEven(2));
+        $this->assertFalse($this->isEven(3));
     }
 }
